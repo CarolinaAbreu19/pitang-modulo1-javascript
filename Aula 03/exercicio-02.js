6a3d6036e2baa0cb12e5431f2c189652b66326c3
Carolina Abreu <anacarolinaks19@gmail.com>
2022-01-28 21:12:42 -0300

    conclusao exercicio 02

---
+++ Aula 03/exercicio-02.js
@@ -0,0 +1,15 @@
+const automovel =  {
+        nome: "Belinda",
+        marca: "Volkswagen",
+        modelo: "Gol",
+        cor: "Preto",
+    getAutomovel() {
+        return [
+            this.nome,
+            this.marca,
+            this.modelo,
+            this.paisOrigem
+        ]
+    }
+};
+
